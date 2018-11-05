4418751cb96b8d946a3a73c47d229f606130650d
Charlotte <cha.perution@gmail.com>
2018-11-05 18:13:37 +0100

    init AdvertController and home view

---
+++ src/Controller/AdvertController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdvertController extends AbstractController
+{
+    /**
+     * @Route("/advert", name="advert")
+     */
+    public function index()
+    {
+        return $this->render('advert/index.html.twig', [
+            'controller_name' => 'AdvertController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+    	return $this->render('advert/home.html.twig');
+    }
+}
